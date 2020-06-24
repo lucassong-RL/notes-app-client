3397f75f9813c97d8b5a3f3f39ba9df79486e4ee
Lucas Song <lucas.song@LucasSong-MBPi.local>
2020-06-24 10:43:01 -0700

    add config

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-lucas"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://4b9f5gylf2.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_d5pm40iuk",
+    APP_CLIENT_ID: "3iopfdiuouqsfbel4c4799as2i",
+    IDENTITY_POOL_ID: "us-east-2:7c1ad306-da44-4e42-960b-3fcb8d4d1557"
+  }
+};
