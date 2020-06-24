c651d81996768b31b2931872c1fac1910782f34a
Lucas Song <lucas.song@LucasSong-MBPi.local>
2020-06-24 14:55:33 -0700

    update redirect

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_51GxKZKCj77DIADOKg9k7qRz8el0pbDovgOKCiPXCgGUBt51HrgVCXNSQr21cxfRvSe2xJDwIXYPkL8nyBFOj0IDD00dI772GaY",
   s3: {
     REGION: "us-east-2",
-    BUCKET: "notes-app-lucas"
+    BUCKET: "lucas-notes-app-api-prod-serverlessdeploymentbuck-2xum7rvze07h"
   },
   apiGateway: {
     REGION: "us-east-2",
-    URL: "https://4b9f5gylf2.execute-api.us-east-2.amazonaws.com/prod"
+    URL: "https://l3dw5okdd8.execute-api.us-east-2.amazonaws.com/prod"
   },
   cognito: {
     REGION: "us-east-2",
     USER_POOL_ID: "us-east-2_d5pm40iuk",
     APP_CLIENT_ID: "3iopfdiuouqsfbel4c4799as2i",
-    IDENTITY_POOL_ID: "us-east-2:7c1ad306-da44-4e42-960b-3fcb8d4d1557"
+    IDENTITY_POOL_ID: "us-east-2:e3c4687b-ca4a-43b8-baf4-ff13106c0c8a"
   }
 };
